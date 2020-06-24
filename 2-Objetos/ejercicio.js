9cc772db5c683883601386d94a4e1eefac782d74
Carla PGavilan <carlapgc@CarlaPGavilans-iMac.local>
2020-06-23 21:52:33 -0500

    exercise penguin

--- 2-Objetos/ejercicio.js
+++ 2-Objetos/ejercicio.js
@@ -1,9 +1,11 @@
+// Elige un pingüino de la lista y crea un objeto
 const myPenguin = {
     character: "Frobisher",
     origin: "Doctor Who",
     creator: "Steve Parkhouse",
     notes: "A humanoid from a shape-changing extraterrestial race who prefers to take the guise of a penguin",
 
+    // Imprime el nombre de pingüino en consola
     printWelcome(){
         console.log(`Hola, soy un pingüino y mi nombre es  ${myPenguin.character}`);
     },
@@ -11,8 +13,12 @@ const myPenguin = {
 };
 
 myPenguin.printWelcome()
-myPenguin.puedeVolar = true
+
+// Línea de código que agrega puedevolar
+myPenguin.puedeVolar = false
 console.log(`Puedo volar ${myPenguin.puedeVolar}`);
+
+// Linea de código para agregar graznar
 myPenguin.graznar =  function() {
     console.log("kaww kaww!!")
 }
